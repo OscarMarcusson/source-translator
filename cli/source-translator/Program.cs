efae1d06559e31f865c58b7f7f050d435cfdb533
OscarMarcusson <oscar.marcusson@outlook.com>
2024-04-14 11:25:37 +0200

    Create base project

---
+++ cli/source-translator/Program.cs
@@ -0,0 +1,45 @@
+﻿if (args.Length == 0 || args[0] == "-h" || args[0] == "--help")
+{
+    PrintHelp();
+    return 0;
+}
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Not implemented");
+Console.ResetColor();
+return 1;
+
+
+
+
+
+
+static void PrintHelp()
+{
+    Console.WriteLine("Source Translator");
+    Console.WriteLine();
+    PrintArg("h", "help", "Prints this dialog");
+    PrintArg("t", "translations", "The translation file");
+    PrintArg("i", "input", "The comma separated input files");
+
+
+
+    static void PrintArg(string shortArg, string longArg, string description)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write($"-{shortArg} ");
+
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.Write("/");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write($" --{longArg}");
+
+        var remainingWhitespace = 24 - Console.CursorLeft;
+        if (remainingWhitespace > 0)
+            Console.Write(new string(' ', remainingWhitespace));
+
+        Console.ResetColor();
+        Console.WriteLine(description);
+    }
+}
